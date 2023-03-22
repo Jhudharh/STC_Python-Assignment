9bb1f0a87092e1a39fd389387d339cbc48d17756
Itah Judah Nduka <judahitah@gmail.com>
2023-03-22 09:17:42 +0100

    Create Lists.py
    Lists updated successfully

---
+++ Lists.py
@@ -0,0 +1,15 @@
+geek = [1,1,2,4,6,3,6,6,23,25,1,7]
+
+duplicate_items = ( ) / set ( )
+
+unique_items = [ ]
+
+for x in geek :
+
+if x not in duplicate_items :
+
+uniq_items.append ( x )
+
+duplicate_items.add ( x ) 
+
+print ( dup_items )
